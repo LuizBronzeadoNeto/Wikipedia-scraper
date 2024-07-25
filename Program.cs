ba72a297ba4c7ffded12e36a893bd957b5b2fe52
Luiz Bronzeado neto <luizbronzeadoneto@gmail.com>
2024-07-24 21:17:14 -0300

    initial

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using HtmlAgilityPack;
+namespace simpleWebScraper
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string? inputLink = Console.ReadLine();
+            HttpClient client = new HttpClient();
+            string html = await client.GetStringAsync(inputLink);
+
+            HtmlDocument document = new HtmlDocument();
+            document.LoadHtml(html);
+
+            var paragraphs = document.DocumentNode.SelectNodes("//p");
+
+            foreach (var paragraph in paragraphs)
+            {
+                Console.WriteLine(paragraph.InnerText);
+            }
+
+        }
+    }
+}
